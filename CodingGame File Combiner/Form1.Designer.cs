09f1373db8b91d4be8222bff011f723e6cfbce53
Zachary Carey <zcarey@ltu.edu>
2020-05-11 11:02:58 -0400

    Finished

--- CodingGame File Combiner/Form1.Designer.cs
+++ CodingGame File Combiner/Form1.Designer.cs
@@ -24,12 +24,92 @@
 		/// </summary>
 		private void InitializeComponent() {
 			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 30);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Source Files";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 68);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 35);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Output File";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(93, 21);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(35, 13);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(93, 79);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "label2";
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(12, 147);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 35);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "Update";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(727, 194);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Button button3;
 	}
 }
 
--- CodingGame File Combiner/Form1.cs
+++ CodingGame File Combiner/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,196 @@ using System.Windows.Forms;
 
 namespace CodingGame_File_Combiner {
 	public partial class Form1 : Form {
+
+		private FolderBrowserDialog openDialog;
+		private SaveFileDialog saveDialog;
+		private FileSystemWatcher watcher;
+
+		private string openDir;
+		private string saveDir;
+
+		private volatile bool filesChanged = false;
+
 		public Form1() {
 			InitializeComponent();
+
+			openDialog = new FolderBrowserDialog();
+			openDir = Properties.Settings.Default.PreviousDirectory;
+			if (!string.IsNullOrEmpty(openDir)) {
+				openDialog.SelectedPath = openDir;
+				CreateWatcher();
+			}
+			openDialog.ShowNewFolderButton = true;
+			label1.Text = openDir;
+
+			saveDialog = new SaveFileDialog();
+			saveDialog.OverwritePrompt = true;
+			saveDialog.RestoreDirectory = true;
+			saveDir = Properties.Settings.Default.SaveDirectory;
+			if (!string.IsNullOrEmpty(saveDir)) {
+				saveDialog.FileName = saveDir;
+			}
+			label2.Text = saveDir;
+
+			lock (this) {
+				filesChanged = true;
+			}
+			timer1.Start();
 		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+			timer1.Stop();
+			DeleteWatcher();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			if ((openDialog.ShowDialog() == DialogResult.OK) && !string.IsNullOrWhiteSpace(openDialog.SelectedPath)) {
+				openDir = openDialog.SelectedPath;
+				Properties.Settings.Default.PreviousDirectory = openDir;
+				Properties.Settings.Default.Save();
+				label1.Text = openDir;
+				CreateWatcher();
+				lock (this) {
+					filesChanged = true;
+				}
+			}
+		}
+
+		private void DeleteWatcher() {
+			try {
+				if (watcher != null) {
+					watcher.EnableRaisingEvents = false;
+					watcher.Changed -= FileWatcher_Event;
+					watcher.Created -= FileWatcher_Event;
+					watcher.Deleted -= FileWatcher_Event;
+					watcher.Dispose();
+				}
+			} catch (Exception) {
+				Console.WriteLine("Error deleting watcher");
+			} finally {
+				watcher = null;
+			}
+		}
+
+		private void CreateWatcher() {
+			try {
+				DeleteWatcher();
+				watcher = new FileSystemWatcher(openDir + "\\");
+				//watcher.Filter = "*.cs";
+				//watcher.NotifyFilter = NotifyFilters.LastWrite;
+				watcher.Changed += FileWatcher_Event;
+				watcher.Created += FileWatcher_Event;
+				watcher.Deleted += FileWatcher_Event;
+				watcher.Renamed += FileWatcher_Event;
+				watcher.EnableRaisingEvents = true;
+			} catch (Exception) {
+				Console.WriteLine("Error creating watcher");
+				DeleteWatcher();
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e) {
+			if (saveDialog.ShowDialog() == DialogResult.OK) {
+				saveDir = saveDialog.FileName;
+				Properties.Settings.Default.SaveDirectory = saveDir;
+				Properties.Settings.Default.Save();
+				label2.Text = saveDir;
+				lock (this) {
+					filesChanged = true;
+				}
+			}
+		}
+
+		private void FileWatcher_Event(object sender, FileSystemEventArgs e) {
+			if (e.Name.Trim().EndsWith(".cs")) {
+				lock (this) {
+					filesChanged = true;
+				}
+			}
+		}
+
+		private void button3_Click(object sender, EventArgs e) {
+			lock (this) {
+				filesChanged = true;
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e) {
+			bool update = false;
+			lock (this) {
+				if (filesChanged) {
+					update = true;
+					filesChanged = false;
+				}
+			}
+			if (update) UpdateFile();
+		}
+
+		private void UpdateFile() {
+			try {
+				FileInfo file = new FileInfo(saveDir);
+				if (Directory.Exists(openDir)) {
+					using (StreamWriter writer = new StreamWriter(file.FullName, false)) {
+						string[] files = Directory.GetFiles(openDir);
+						if (!CombineFiles(files, writer)) throw new Exception("Error while combining.");
+					}
+				}
+			} catch (Exception ex) {
+				//if (sender != this) {
+				Console.WriteLine("ERROR: {0}", ex.Message);
+				MessageBox.Show("Unable to combine or save files!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				//}
+			}
+		}
+
+		private static bool CombineFiles(string[] files, StreamWriter output) {
+			try {
+				List<string> usings = new List<string>();
+				List<string> sources = new List<string>();
+				foreach (string file in files.Where(x => x.Trim().EndsWith(".cs"))) {
+					using (StreamReader reader = new StreamReader(File.OpenRead(file))) {
+						if (!GetInfo(reader, usings, sources)) {
+							return false;
+						}
+					}
+				}
+
+				foreach (string u in usings) {
+					output.WriteLine(u);
+				}
+				output.WriteLine();
+				foreach (string source in sources) {
+					output.WriteLine(source);
+				}
+				return true;
+			} catch (Exception) {
+				return false;
+			}
+		}
+
+		private static bool GetInfo(StreamReader reader, List<string> usings, List<string> sources) {
+			string source = reader.ReadToEnd();
+			foreach (string line in source.Split('\n')) {
+				string str = line.Trim();
+				if (str.StartsWith("using")) {
+					if (!usings.Contains(str)) {
+						usings.Add(str.TrimEnd());
+					}
+				} else {
+					sources.Add(line.TrimEnd());
+				}
+			}
+			/*
+						int index = source.IndexOf("namespace");
+						int bracket1 = source.IndexOf('{', index);
+						int bracket2 = source.LastIndexOf('}');
+						if((bracket1 > index) && (bracket2 > bracket1)) {
+							source = source.Substring(bracket1 + 1, bracket2 - bracket1 - 1);
+
+						}
+						*/
+			return true;
+		}
+
 	}
 }
